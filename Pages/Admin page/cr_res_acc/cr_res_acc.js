91cb6d7fb66e6f3174982b537a52365e9c45a9d0
Nick Belibasakis <nikosbelibasakis71@gmail.com>
2024-08-18 23:19:25 +0300

    CRA version 2

--- Pages/Admin page/cr_res_acc/cr_res_acc.js
+++ Pages/Admin page/cr_res_acc/cr_res_acc.js
@@ -1,5 +1,5 @@
- // Function to toggle password visibility
- function togglePasswordVisibility() {
+// Function to toggle password visibility
+function togglePasswordVisibility() {
     const passwordField = document.getElementById('password');
     const showPasswordCheckbox = document.getElementById('showPassword');
     if (showPasswordCheckbox.checked) {
@@ -18,3 +18,10 @@ if (username) {
     mainPageLink.href = `../admin_page.html?username=${encodeURIComponent(username)}`;
 }
 
+// Prevent form submission from doing anything and clear the form fields
+document.getElementById('rescuerAccountForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Αποτρέπει οποιαδήποτε ενέργεια να γίνει κατά την υποβολή της φόρμας
+
+    // Καθαρίζει όλα τα πεδία της φόρμας
+    document.getElementById('rescuerAccountForm').reset();
+});
