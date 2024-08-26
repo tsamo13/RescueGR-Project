a415267b09ee3e0f594673294d4460e8bb1fd3bf
tsamo13 <tsamoangel2001@gmail.com>
2024-08-26 20:43:48 +0300

    Sign_up last commit

--- Pages/Sign_up page/sign_up.js
+++ Pages/Sign_up page/sign_up.js
@@ -22,6 +22,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Declare a marker variable (initially undefined)
     let marker;
 
+    let locationSelected = false;
+
     // Update the marker's location and hidden fields when the user clicks on the map
     map.on('click', function (e) {
         // If the marker doesn't exist, create it
@@ -35,11 +37,19 @@ document.addEventListener('DOMContentLoaded', function () {
         marker.setLatLng(e.latlng);
         document.getElementById('latitude').value = e.latlng.lat;
         document.getElementById('longitude').value = e.latlng.lng;
+
+        locationSelected = true;
     });
 
     // Step 1.3: Handle form submission
     document.getElementById('signupForm').addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission
+
+        if (!locationSelected) {
+            Swal.fire({title: 'Error!', text:'Please select your location on the map before signing up.', icon: 'error', confirmButtonText: 'OK'});
+            return;
+        }
+        
 
         const formData = {
             name: document.getElementById('name').value,
@@ -60,14 +70,16 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             if (data.success) {
-                window.location.href = 'login_reg_page.html'; // Redirect to login page on success
+                Swal.fire({title: 'Success!', text:'Civilian account created successfully!', icon: 'success', confirmButtonText: 'OK'});
+                document.getElementById('signupForm').reset();
+
             } else {
-                alert('Sign up failed: ' + data.message);
+                Swal.fire({title: 'Error!', text:'This username already exists!', icon: 'error', confirmButtonText: 'OK'});
             }
         })
         .catch(error => {
             console.error('Error:', error);
-            alert('An error occurred while signing up.');
+            Swal.fire({title: 'Error!', text:'An error occurred while signing up. Please try again', icon: 'error', confirmButtonText: 'OK'});
         });
     });
 });
